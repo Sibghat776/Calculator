fde7f735256db0d730c13ea96bd5d4f0eb2c3779
Sibghat776 <ullahsibghat786@gmail.com>
2025-01-16 18:05:50 +0500

    UI end

---
+++ app.js
@@ -0,0 +1,12 @@
+let inputVal = document.querySelector("input").value
+ function calc(inputVal) {
+    console.log(inputVal)
+    switch (inputVal) {
+        case "":
+            
+            break;
+    
+        default:
+            break;
+    }
+}
